cabbfa4a763a65cfa5b7052d43dbd61578bd0bfa
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-01-02 18:01:14 -0300

    add table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento</td>
+              <td>
+                <PriceHighlight variant='income'>
+                  R$ 12.000,00
+                </PriceHighlight>
+                </td>
+              <td>Venda</td>
+              <td>3/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">MC Donalds</td>
+              <td> 
+                <PriceHighlight variant='outcome'>
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>3/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+     
     </div>
   )
 }
