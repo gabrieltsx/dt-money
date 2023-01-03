7abc918bb2e565e306fb89051b3f77673a46291f
Gabriel <gabrielhenrique.ls96@gmail.com>
2023-01-03 15:09:27 -0300

    add axios and feature search

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContext } from "react";
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -11,13 +13,14 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { fetchTransactions } = useContext(TransactionsContext)
+
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
         resolver: zodResolver(searchFormSchema)
     })
 
     async function handleSearchTransactions(data: SearchFormInputs) {
-        // await new Promise(resolve => setTimeout(resolve, 3000))
-        console.log(data)
+        await fetchTransactions(data.query)
     }
 
     return (
